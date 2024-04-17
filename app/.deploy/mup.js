e96fb0980b390cad60b2d89feb803881ec374c91
Brandon Nakata <122415357+bhnakata@users.noreply.github.com>
2024-04-16 21:16:10 -1000

    Changed mup.js so the domain name is https://businessbonders.xyz

--- app/.deploy/mup.js
+++ app/.deploy/mup.js
@@ -1,7 +1,7 @@
 module.exports = {
   servers: {
     one: {
-      host: '164.92.119.232',
+      host: 'businessbonders.xyz',
       username: 'root',
       password: '3Business-Bonder'
     }
@@ -9,17 +9,24 @@ module.exports = {
   app: {
     // if you edit the app 'name' field, be sure to run 'mup stop' if the app is already running.
     // otherwise you will have two apps deployed at once, with unpredictable results.
-    name: 'meteor-react-bootstrap-template',
+    name: 'businessbonders',
     path: '../',
     servers: { one: {}, },
     buildOptions: { serverOnly: true },
     env: {
-      ROOT_URL: 'http://164.92.119.232',
+      ROOT_URL: 'https://businessbonders.xyz',
       MONGO_URL: 'mongodb://mongodb/meteor',
       MONGO_OPLOG_URL: 'mongodb://mongodb/local',
     },
     docker: { image: 'zodern/meteor:latest' },
     enableUploadProgressBar: true
+  },
+  proxy: {
+    domains: 'businessbonders.xyz',
+    ssl: {
+      letsEncryptEmail: 'bhnakata@hawaii.edu',
+      forceSSL: true
+    }
   },
   mongo: { version: '5.0', servers: { one: {} }
   },
